66c31a23e13cdfd15cfae70882ab1494270ba78e
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2021-03-06 11:40:31 -0500

    adds example react component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Person } from './components/Person';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    hello
   </React.StrictMode>,
   document.getElementById('root')
 );
