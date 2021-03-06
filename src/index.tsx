34c2d8964b0dd4b315e83db0a315c31c61db8ba3
Matthew McCracken <mccracm@mcmaster.ca>
2021-03-06 12:20:55 -0500

    Slider panel added

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Header } from './components/sections/Header';
+import { SliderPanel } from './components/SliderPanel';
 import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Header/>
+      <Header />
+      <SliderPanel />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
