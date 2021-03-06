7779e46000c443a9409361275c2a8d901e3570eb
bentvelj <jaredbentvelsen@gmail.com>
2021-03-06 14:20:17 -0700

    playground to create and test models

---
+++ simulation.py
@@ -0,0 +1,17 @@
+
+def S(t): # Susceptible
+    pass    
+
+def I(t): # Infected
+    pass
+
+def R(t): # Recovered
+    pass
+
+def β(t): # Infectivity
+    # β is the average number of individuals that one infected individual will infect per time unit
+    pass
+
+def γ(t): # Recovery time
+    # γ is the rate of recovery, so that 1/γ is the average time period during which an infected individual remains infectious
+    pass
