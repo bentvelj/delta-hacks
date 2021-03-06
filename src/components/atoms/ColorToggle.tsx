6c7f81c2d43521ae544e5e6f0c642fb017fda44d
Chris Andrade <chrisfandrade16@gmail.com>
2021-03-06 14:45:52 -0500

    Adding sun-moon icon to colour toggle switch.

--- src/components/atoms/ColorToggle.tsx
+++ src/components/atoms/ColorToggle.tsx
@@ -1,13 +1,40 @@
 import { Switch, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import React from 'react';
+import styled from 'styled-components';
 
 interface ColorToggleProps {}
 
 export const ColorToggle: React.FC<ColorToggleProps> = ({}) => {
   const { colorMode, toggleColorMode } = useColorMode();
+
+  if(colorMode === "light") {
+    return (
+      <FlexContainer>
+        <StyledWrapper><SunIcon/></StyledWrapper>
+        <StyledWrapper><StyledSwitch id="lightdark" size="lg" onChange={toggleColorMode}/></StyledWrapper>
+      </FlexContainer>
+    );
+  }
+
   return (
-    <Switch id="lightdark" size="lg" onChange={toggleColorMode}>
-      Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-    </Switch>
+    <FlexContainer>
+        <StyledWrapper><MoonIcon/></StyledWrapper>
+        <StyledWrapper><StyledSwitch id="lightdark" size="lg" onChange={toggleColorMode}/></StyledWrapper>
+      </FlexContainer>
   );
 };
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-item: center;
+`;
+
+const StyledWrapper = styled.div`
+
+`;
+
+const StyledSwitch = styled(Switch)`
+  margin-left: 7px;
+`;
