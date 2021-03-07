93513625973483e489301ce38ee0306021e91958
Matthew McCracken <mccracm@mcmaster.ca>
2021-03-07 08:02:56 -0500

    legend file

---
+++ src/components/atoms/Legend.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface LegendProps {}
+
+export const Legend: React.FC<LegendProps> = ({}) => {
+  return <div></div>;
+};
+
+const styledOuterBox = styled.div`
+  width: 200px;
+  height: 400px;
+`;
