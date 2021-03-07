46a94914bb501c5716d8ee8c7ada4bcedd8236d3
bentvelj <jaredbentvelsen@gmail.com>
2021-03-06 18:39:49 -0700

    added modeling to account for masks

--- simulation.py
+++ simulation.py
@@ -1,20 +1,66 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
-def S(t): # Susceptible
-    pass    
+N = 1000 # Initial population
+R0 = 0 # Initial recovered - immune or dead
+I0 = 3 # Initial infect
+S0 = N - I0 - R0 # Initial susceptible
+B = 0.2 # Infections per unit time                      
+Y = 1.0/10 # Recovery Time
+D = 0.2 # Death Rate
+SD_C = 0 # Social-distancing compliance, 0 = none, 1 = heavy social distancing
+MW_C = 0 # Mask-wearing compliance, 0 = no masks, 1 = everyone wears mask
 
-def I(t): # Infected
-    pass
+# Assume masks reduce by 50% both ways?
+E_IN = 0.5
+E_OUT = 0.5
 
-def R(t): # Recovered
-    pass
+# Unsimplified
+# MASK_FACTOR = 1*(1-MW_C)**2 + (E_OUT*1*)*MW_C*(1-MW_C) + (1*E_IN)*MW_C*(1-MW_C) + E_IN*E_OUT*MW_C**2;
 
-def β(t): # Infectivity
-    # β is the average number of individuals that one infected individual will infect per time unit
-    pass
+# Simplified
+# Relative chance of infectivition accouting for masks (%)
+# Eg. No masks would mean MASK_FACTOR = 1 (no change), everyone wearing 100% effective masks => MASK_FACTOR = 0 (zero transmission)
+MASK_FACTOR = (1-MW_C)**2 + E_OUT*MW_C*(1-MW_C) + E_IN*MW_C*(1-MW_C) + E_IN*E_OUT*MW_C**2;
 
-def γ(t): # Recovery time
-    # γ is the rate of recovery, so that 1/γ is the average time period during which an infected individual remains infectious
-    pass
+# Reduction in infectivity due to masks as % would be 1 - MASK_FACTOR
 
+# Just do this?
+B = B*MASK_FACTOR
 
-#The product βS(t) I(t) is the total infection rate, the fraction of the population that will be infected per unit time at time t.
+# A grid of time points (in days)
+t = np.linspace(0, 160, 160)
+
+def derivative(y, t, N, beta, gamma):
+    S, I, R = y
+    dSdt = -beta * S * I / N
+    dIdt = beta * S * I / N - gamma * I
+    dRdt = gamma * I
+    return (dSdt, dIdt, dRdt)
+
+# Initial conditions vector
+y0 = (S0, I0, R0)
+# Integrate the SIR equations over the time grid, t.
+ret = odeint(derivative, y0, t, args=(N, B, Y))
+S, I, R = ret.T
+
+# Plot the data on three separate curves for S(t), I(t) and R(t)
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
+#ax.plot(t, (R/1000), 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.plot(t, (R/1000)*0.8, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.plot(t, (R/1000)*0.2, 'k', alpha=0.5,lw=2, label='Fockin Dead')
+ax.set_xlabel('Time /days')
+ax.set_ylabel('Number (1000s)')
+ax.set_ylim(0,1.2)
+ax.yaxis.set_tick_params(length=0)
+ax.xaxis.set_tick_params(length=0)
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+for spine in ('top', 'right', 'bottom', 'left'):
+    ax.spines[spine].set_visible(False)
+plt.savefig('foo2.png')
