0305402f583ac4a6d011b95351fd971ca4d13cc5
Matthew McCracken <mccracm@mcmaster.ca>
2021-03-06 22:44:13 -0500

    finished colours for provinces

--- src/components/atoms/Projection.tsx
+++ src/components/atoms/Projection.tsx
@@ -32,12 +32,14 @@ export const Projection: React.FC<ProjectionProps> = ({
         <StyledGeographies geography={geoUrl} className="g-geographies">
           {({ geographies }) =>
             geographies.map((geo, i) => {
+              // console.log(i);
               return (
                 <StyledProvince
                   caseGradient={caseGradient}
                   numberList={numberList}
                   province={province}
                   key={i}
+                  keyVal={i}
                   geography={geo}
                   stroke={theme.colors.gray[300]}
                   strokeWidth="0.5px"
@@ -80,12 +82,14 @@ const StyledProvince = styled(Geography)<{
   province: string;
   caseGradient: boolean;
   numberList: number[];
-  key: number;
+  keyVal: number;
 }>`
   fill: ${(props) => {
     if (props.caseGradient) {
-      console.log(props.numberList[props.key]);
-      return theme.colors.red[300];
+      return colourSelector(props.numberList[props.keyVal]);
+      // return `rgb(${props.numberList[props.keyVal]}, ${
+      //   255 - props.numberList[props.keyVal]
+      // }, 0)`;
     }
   }};
   :hover {
@@ -113,3 +117,27 @@ const StyledProvince = styled(Geography)<{
     }};
   }
 `;
+
+function colourSelector(keyValue: number) {
+  if (keyValue < 10) {
+    return theme.colors.green[200];
+  } else if (keyValue >= 10 && keyValue < 20) {
+    return theme.colors.green[600];
+  } else if (keyValue >= 20 && keyValue < 30) {
+    return theme.colors.green[800];
+  } else if (keyValue >= 30 && keyValue < 40) {
+    return theme.colors.yellow[200];
+  } else if (keyValue >= 40 && keyValue < 50) {
+    return theme.colors.yellow[400];
+  } else if (keyValue >= 50 && keyValue < 60) {
+    return theme.colors.yellow[600];
+  } else if (keyValue >= 60 && keyValue < 70) {
+    return theme.colors.orange[400];
+  } else if (keyValue >= 70 && keyValue < 80) {
+    return theme.colors.orange[700];
+  } else if (keyValue >= 80 && keyValue < 90) {
+    return theme.colors.red[600];
+  } else if (keyValue >= 90) {
+    return theme.colors.red[800];
+  }
+}
